fbc67629af254fb33d864ff414cfb71b36f248e7
Your Name <you@example.com>
2022-05-18 12:49:27 -0300

    adicionando feature de cadastro, edicao e listagem e remocao de clientes

--- src/server.js
+++ src/server.js
@@ -1,30 +1,32 @@
 const express = require('express')
 const path = require('path')
 
+const db = require('./database')
+const routes = require('./routes')
+
 const app = express()
 
-//definindo o template engine
+// conexão com o banco de dados
+db.connect()
+
+// definindo o template engine
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-//definindo os arquivos públicos
+// definindo os arquivos públicos
 app.use(express.static(path.join(__dirname, 'public')))
 
-//habilita server para receber dados via post form
+// habilita server para receber dados via post (formulário)
 app.use(express.urlencoded({ extended: true }))
 
-//rotas
-app.get ('/', (req, res) => {
-    res.render('index', {
-        title: 'titulo teste'
-    })
-})
+// definindo as rotas
+app.use('/', routes)
 
 // 404 error (not found)
-app.use((req, res) => { //middleware
-    res.send('Página não encontrada')
+app.use((req, res) => { // middleware
+  res.send('Página não encontrada!')
 })
 
-//executando o servidor
+// executando o servidor
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
